6f683e7243b80de6d2a5174505204dc01a83d6c0
cerneiirina <44048781+cerneiirina@users.noreply.github.com>
2019-03-07 12:38:42 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace lab2
+{
+    class Program
+    {
+        static AutoResetEvent waitHandler = new AutoResetEvent(false);
+
+        static Thread first;
+        public static void SecondThread()
+        {
+            Console.WriteLine("Second thread starts");
+            waitHandler.Set();
+        }
+        public static void ThirdThread()
+        {
+            Console.WriteLine("Third thread starts");
+            waitHandler.WaitOne();
+            Thread fifth = new Thread(() => FifthThread());
+            fifth.Start();
+        }
+
+        public static void FifthThread()
+        {
+            Console.WriteLine("Fifth thread start");
+            Console.ReadKey();
+        }
+
+        public static void FourthThread()
+        {
+            Console.WriteLine("Fourth thread start");
+            waitHandler.Set();
+        }
+
+        public static void firstThread()
+        {
+            Console.WriteLine("First thread started");
+            Thread secondTh = new Thread(() => SecondThread());
+            Thread thirdTh = new Thread(() => ThirdThread());
+            Thread fourthTh = new Thread(() => FourthThread());
+            secondTh.Start();
+            waitHandler.WaitOne();
+            thirdTh.Start();
+            fourthTh.Start();
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            
+            first = new Thread(() => firstThread());
+            first.Start();
+        }
+    }
+}
